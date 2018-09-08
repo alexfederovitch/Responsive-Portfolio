012846d56026821f0c626261ed8c72a35f247c7d
Alex Federovitch <alexfederovitch@yahoo.com>
2018-09-08 00:22:08 -0500

    Updating portfolio page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,10 @@
 
+$(window).on("load", function() {
+    $(".loader .inner").fadeOut(500, function() {
+        $(".loader").fadeOut(750);
+    });
+})
+
 $(document).ready(function(){
     
     $('#slides').superslides({
@@ -34,7 +40,6 @@ $(document).ready(function(){
         }
     })
 
-
     let skillsTopOffset = $('.skillsSection').offset().top;
     
     $(window).scroll(function() {
@@ -53,7 +58,7 @@ $(document).ready(function(){
              });
 
         }
-    })
+    });
 
     $('[data-fancybox]').fancybox();
 
@@ -65,5 +70,53 @@ $(document).ready(function(){
             queue: false
         }
     });
+
+    $("#filters a").click(function() {
+
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        let selector = $(this).attr("data-filter");
+
+        $('.items').isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        return false;
+
+    });
+    
+    $("#navigation li a").click(function(e) {
+        e.preventDefault();
+
+        let targetElement = $(this).attr("href");
+        let targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({ scrollTop: targetPosition - 50}, "slow");
+    });
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        let body = $("body");
+
+        if ($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px")
+            body.addClass("fixedNav")
+        }
+        else {
+            body.css("padding-top", 0)
+            body.removeClass("fixedNav");
+        }
+
+    };
 
 });
